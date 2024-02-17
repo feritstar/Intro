3260dbd5edeaa6705dba9584f75162f4cec47f53
FeritYILDIZ <feritstar@gmail.com>
2024-02-18 00:09:53 +0300

    Add project files.

---
+++ Business/CourseManager.cs
@@ -0,0 +1,49 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+    public Course[] courses = new Course[3];
+
+    public CourseManager()
+    {
+        Course course1 = new Course()
+        {
+            Id = 1,
+            Name = "A",
+            Description = "A V00.01",
+            Price = 15
+        };
+
+        Course course2 = new Course()
+        {
+            Id = 2,
+            Name = "B",
+            Description = "B V00.02",
+            Price = 10
+        };
+
+        Course course3 = new Course()
+        {
+            Id = 3,
+            Name = "C",
+            Description = "C V00.03",
+            Price = 20
+        };
+
+        courses[0] = course1;
+        courses[1] = course2;
+        courses[2] = course3;
+    }
+
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Intro.Business;
+using Intro.Entities;
+
+IndividualCustomer customer1 = new IndividualCustomer();
+customer1.Id = 1;
+customer1.NationalIdentity = "12345678910";
+customer1.FirstName = "TestFirstName";
+customer1.LastName = "TestSurname";
+customer1.CustomerNumber = "123456";
+
+IndividualCustomer customer2 = new IndividualCustomer();
+customer2.Id = 2;
+customer2.NationalIdentity = "98765432110";
+customer2.FirstName = "TestFirstName2";
+customer2.LastName = "TestSurname2";
+customer2.CustomerNumber = "123457";
+
+CorporateCustomer customer3 = new CorporateCustomer();
+customer3.Id = 3;
+customer3.CompanyName = "Kodlamaio";
+customer3.CustomerNumber = "654778";
+customer3.TaxNumber = "12345678987";
+
+CorporateCustomer customer4 = new CorporateCustomer();
+customer4.Id = 4;
+customer4.CompanyName = "Abc";
+customer4.CustomerNumber = "654779";
+customer4.TaxNumber = "12345678985";
+
+int number1 = 10;//20
+int number2 = 20;
+number1 = number2;
+number2 = 50;
+Console.WriteLine(number1);
+
+string[] cities1 = { "Ankara", "İstanbul", "İzmir" };
+string[] cities2 = { "Bursa", "Bolu", "Diyarbakır" };
+
+cities2 = cities1;
+cities1[0] = "Adana";
+
+Console.WriteLine(cities2[0] + "\n");
+
+//value types -->int, bool,double...
+//reference types --> array, class, interface...
+//101        //102     //103      //104
+BaseCustomer[] customers = { customer1, customer2, customer3, customer4 };
+
+//polymorphism
+foreach (BaseCustomer customer in customers)
+{
+	if (customer is IndividualCustomer)
+	{
+		Console.WriteLine(((IndividualCustomer)customer).FirstName + "...");
+	}
+	else if (customer is CorporateCustomer)
+	{
+        Console.WriteLine(((CorporateCustomer)customer).CompanyName + "...");
+    }
+
+    Console.WriteLine(customer.CustomerNumber + "\n");
+}
